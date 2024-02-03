17ebae595038484661e937ed377a16464471cb6c
swagath shetty <swagath.shetty@gmail.com>
2024-02-03 10:38:42 +0530

    successfully setup config and DB module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose'
+import {ConfigService} from '@nestjs/config'
+import { ConfigModule } from '../config/config.module';
+
 
 @Module({
-    imports:[MongooseModule.forRoot('mongodb://localhost/sleepr')]
+    imports:[MongooseModule.forRootAsync({
+        imports: [ConfigModule], 
+        useFactory: (configService:ConfigService)=>{
+            console.log(configService.get('MONGODB_URI'))
+            return {
+                uri:configService.get('MONGODB_URI')
+            }
+        },
+        inject:[ConfigService]
+    })]
 })
 export class DatabaseModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from '@app/common/database/database.module';
+import { DatabaseModule } from '@app/common';
 
 @Module({
   imports: [DatabaseModule],

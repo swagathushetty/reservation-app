01fc5496072ceb3901d5647363d555792e431875
swagath shetty <swagath.shetty@gmail.com>
2024-02-05 16:08:42 +0530

    implemented create in abstract clas

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,9 +1,19 @@
-import { Model } from "mongoose";
+import { Model, Types } from "mongoose";
 import { AbstractDocument } from "./abstract.schema";
 
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument>{
     constructor(protected readonly model:Model<TDocument>){
-        
+
+    }
+
+    async create(document: Omit<TDocument,'_id'>):Promise<TDocument>{
+
+        const createdDocument = new this.model({
+            ...document,
+            _id:new Types.ObjectId()
+        })
+
+        return (await createdDocument.save()).toJSON() as unknown as TDocument
     }
 }

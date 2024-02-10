e09c6d29ec0246cb3fbff6e17f603232e10aadc1
swagath shetty <swagath.shetty@gmail.com>
2024-02-10 16:45:20 +0530

    completed the abstract DB repository

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,10 +1,11 @@
-import { FilterQuery, Model, Types } from "mongoose";
+import { FilterQuery, Model, Types, UpdateQuery } from "mongoose";
 import { AbstractDocument } from "./abstract.schema";
-import { filter } from "rxjs";
 import { MaxFileSizeValidator, NotFoundException } from "@nestjs/common";
-
+import { Logger } from "@nestjs/common";
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument>{
+    protected abstract readonly logger: Logger;
+
     constructor(protected readonly model:Model<TDocument>){
 
     }
@@ -28,5 +29,38 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument>{
         }
 
         return document
+    }
+
+
+    async findOneAndUpdate( 
+        filterQuery: FilterQuery<TDocument>,
+        update: UpdateQuery<TDocument>
+        ):Promise<TDocument>
+        {
+
+        const document = this.model.findOneAndUpdate(filterQuery,update,{
+            new:true
+        }).lean<TDocument>(true)
+
+
+        if(!document){
+            this.logger.warn('no document found with the filter query',MaxFileSizeValidator)
+            throw new NotFoundException('document was not found')
+        }
+
+        return document
+       
+    }
+
+
+    async find(filterQuery: FilterQuery<TDocument>):Promise<TDocument>{
+
+        return await this.model.find(filterQuery).lean<TDocument>(true);
+    }
+
+
+    async findOneAndDelete(filterQuery: FilterQuery<TDocument>):Promise<TDocument>{
+
+        return await this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
     }
 }
